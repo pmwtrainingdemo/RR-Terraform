5ec13d31ffa12a5fe6588a4d561f8f9d7c360ce9
pmwtrainingdemo <154430919+pmwtrainingdemo@users.noreply.github.com>
2023-12-22 10:43:28 +0000

    v2.1

--- Code/lab1.9/provider.tf
+++ Code/lab1.9/provider.tf
@@ -1,14 +1,14 @@
-	required_providers {
-	azurerm = {
-	source = "hashicorp/azurerm"
-	version = "~>3.1"
-	}
-	}
-	}
-	
-	provider "azurerm" {
-	features {}
-	
-	storage_use_azuread = true
-	}
-s
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  storage_use_azuread = true
+}
